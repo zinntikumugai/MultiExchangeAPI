e8c3c9ab0576148aa54af661008e83e6904d1303
zinntikumugai <uesitananame55@gmail.com>
2018-01-14 21:48:02 +0900

    base

---
+++ Model/BaseModel.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once __DIR__ ."/../vendor/autoload.php";
+require_once __DIR__ ."/Util.php";
+
+use mpyw\Co\Co;
+use mpyw\Co\CURLException;
+
+/**
+ *
+ */
+class BaseModel {
+
+    const NAME = null;
+    const APIVER = null;
+    const SYMBOL = '_'; //Defalt Symbol "_"
+    const BASEURL = null;
+
+    function __construct($arg = []) {
+    }
+
+    /*
+    * デバック用、接続先取引場名 <- マーケット
+    */
+    public static function getAccessEcho($marketName) {
+        return self::Name ." <- " .$marketName;
+    }
+
+    public static function makeMarket($fast, $second) {
+        return $fst .self::SYMBOL .$second;
+    }
+
+    /*
+    * 表示用、桁区切りと小数点以下の不要0の除去
+    */
+    public static function shapingAmount($amount = 0, $shaping = 3) {
+        $amount = number_format($amount, $shaping);
+        $amount = preg_replace("/\.?0+$/",'',$amount);
+        return $amount;
+    }
+
+
+}
+
+
+?>
